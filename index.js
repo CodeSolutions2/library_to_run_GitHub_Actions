afcdd8c94e13eb2fff8fcd443c76931ae27fcd89
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-06-18 17:49:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import { run_backend_process } from "./library_to_run_GitHub_Actions.js";
+import { run_backend_process } from "./dist/library_to_run_GitHub_Actions.js";
 window.run_backend_process = run_backend_process;
