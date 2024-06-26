0ddea8a3014cf4b79d84c3415e3690c90bd5d021
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-06-26 15:28:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import { run_backend_process } from "./dist/library_to_run_GitHub_Actions.js";
+import { run_backend_process, decode_desalt, PUT_create_a_file_RESTAPI, PUT_add_to_a_file_RESTAPI,  GET_text_from_file_wo_auth_GitHub_RESTAPI, GET_fileDownloadUrl_and_sha, rand_perm } from "./library_to_run_GitHub_Actions.js";
 window.run_backend_process = run_backend_process;
+window.decode_desalt = decode_desalt;
+window.PUT_create_a_file_RESTAPI = PUT_create_a_file_RESTAPI;
+window.PUT_add_to_a_file_RESTAPI = PUT_add_to_a_file_RESTAPI;
+window.GET_text_from_file_wo_auth_GitHub_RESTAPI = GET_text_from_file_wo_auth_GitHub_RESTAPI;
+window.GET_fileDownloadUrl_and_sha = GET_fileDownloadUrl_and_sha;
+window.rand_perm = rand_perm;
